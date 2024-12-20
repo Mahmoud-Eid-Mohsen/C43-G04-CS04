1a0b82391b88e4b81103ba097bddf220e0ed80da
admin <admin@DESKTOP-JKD834T>
2024-12-20 17:03:52 +0200

    Q1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1 
+            //1- Write a program that allows the user to insert an integer then print all numbers between 1 to
+            //that number.
+            Console.WriteLine("enter number");
+           int n=Convert.ToInt32(Console.ReadLine());   
+           for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+
+
+            }
+            #endregion
         }
     }
 }
